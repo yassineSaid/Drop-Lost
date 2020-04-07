f76037b687402c53bbd755448bc625e18ec3534e
MedAliMezni <54766655+MedAliMezni@users.noreply.github.com>
2020-04-07 19:21:45 +0100

    added cors and axios

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const path = require("path");
+const cors = require("cors");
 const bodyparser = require('body-parser');
 const app = express();
+app.use(cors());
 var usersRouter = require('./routes/api/users');
 
 // Connect Database
